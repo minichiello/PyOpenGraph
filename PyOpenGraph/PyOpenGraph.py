cc1fa7239b7892fd1cfaf8df240e285412258a89
Gerson Minichiello <gerson.minichiello@gmail.com>
2010-05-04 22:03:23 -0300

    first commit

---
+++ PyOpenGraph/PyOpenGraph.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+#Copyright (c) 2010 Gerson Minichiello
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+import urllib
+from HTMLParser import HTMLParser
+
+class PyOpenGraph(object):
+   
+    types = {'activity':['activity', 'sport'],
+        'business':['bar', 'company', 'cafe', 'hotel', 'restaurant'],
+        'group':['cause' 'sports_league' 'sports_team'],
+        'organization':['band', 'government', 'non_profit', 'school', 'university'],
+        'person':['actor', 'athlete', 'author', 'director', 'musician', 'politician', 'public_figure'],
+        'place':['city', 'country', 'landmark', 'state_province'],
+        'product':['album', 'book', 'drink', 'food', 'game', 'isbn', 'movie', 'product', 'song', 'tv_show', 'upc'],
+        'website':['article', 'blog', 'website']}
+    
+    def __init__(self, url):
+        f = urllib.urlopen(url)
+        contents = f.read()
+        f.close()
+        p = PyOpenGraphParser()
+        p.feed(contents)
+        p.close()
+        self.metadata = p.properties
+    
+    def is_valid(self):
+        required = set(['title', 'type', 'image', 'url'])
+        if (set(self.metadata.keys()).intersection(required)) == required:
+            return True
+        else:
+            return False
+    
+    def __str__(self):
+        return self.metadata['title']
+    
+class PyOpenGraphParser(HTMLParser):
+    
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.properties = {}
+        self.value_temp = ''
+    
+    def handle_starttag(self, tag, attrs):
+        if tag == 'meta':
+            for attr, value in attrs:
+                if attr == 'property' and value.startswith('og:'):
+                    self.value_temp = value.replace('og:', '')
+                if attr == 'content':
+                    if self.value_temp != '':
+                        self.properties[self.value_temp] = value
+                        self.value_temp = ''
+
+    def handle_endtag(self, tag):
+        pass
+    
+    def error(self, msg):
+        pass
+
+if __name__ == '__main__':
+    # Usage
+    og = PyOpenGraph('http://www.rottentomatoes.com/m/10011268-oceans/')
+    print og.metadata
+    print og.metadata['title']
---
+++ setup.py
@@ -0,0 +1,55 @@
+from distutils.core import setup
+
+setup(
+    name = 'PyOpenGraph',
+    version = '0.1',
+    description = 'PyOpenGraph is a library written in Python for parsing Open Graph protocol information from web sites.',
+    author = 'Gerson Minichiello',
+    author_email = 'gerson.minichiello@gmail.com',
+    url='http://github.com/minichiello/PyOpenGraph',
+    download_url = 'http://pypi.python.org/pypi/PyOpenGraph',
+    platforms = 'Any',
+    license = 'MIT License',
+    long_description='''\
+=============
+ PyOpenGraph
+=============
+    
+PyOpenGraph is a library written in Python for parsing Open Graph protocol information from web sites.
+
+Learn more about the protocol at:
+
+http://opengraphprotocol.org
+
+--------------
+ Installation
+--------------
+
+To install, download the archive at http://pypi.python.org/pypi/PyOpenGraph and decompress, run python setup.py install.
+
+-------
+ Usage
+-------
+::
+
+    import PyOpenGraph
+
+    og = PyOpenGraph('http://www.rottentomatoes.com/m/10011268-oceans/')
+
+    print og.metadata # => {'url': 'http://www.rottentomatoes.com/m/10011268-oceans/', 'site_name': 'Rotten Tomatoes', 'image': 'http://images.rottentomatoes.com/images/movie/custom/68/10011268.jpg', 'type': 'movie', 'title': 'Oceans'}
+
+    print og.metadata['title'] # => Oceans
+
+    og.is_valid # => return True or False
+''',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
